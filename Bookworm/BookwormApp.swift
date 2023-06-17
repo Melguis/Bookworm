cf27d4f239ff753f012f4b3f54b7fb52bc72bbf8
Jorge Henrique <jorgehenriquedasilva2014@hotmail.com>
2023-06-17 09:58:35 -0300

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Jorge Henrique on 17/06/23.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
